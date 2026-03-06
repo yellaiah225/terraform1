d981d12b304366f243b65fc9d88eeab7d0beb7b7
yellaiah225 <137243250+yellaiah225@users.noreply.github.com>
2026-03-06 16:43:31 +0530

    Create variables.tf for Azure Linux VM creation

--- variables.tf
+++ variables.tf
@@ -1,51 +1,54 @@
-# Terraform Variables for Azure VM Configuration
-
-variable "resource_group" {
+variable "resource_group_name" {
   description = "The name of the resource group"
-  type        = string
+  type = string
 }
 
 variable "location" {
-  description = "The Azure location where the VM will be created"
-  type        = string
+  description = "The Azure location where the resources will be created"
+  type = string
 }
 
 variable "vm_name" {
   description = "The name of the virtual machine"
-  type        = string
+  type = string
 }
 
-variable "image_publisher" {
-  description = "The publisher of the image to be used for the VM"
-  type        = string
+variable "vm_size" {
+  description = "The size of the virtual machine"
+  type = string
 }
 
-variable "image_offer" {
-  description = "The offer of the image to be used for the VM"
-  type        = string
-}
-
-variable "image_sku" {
-  description = "The SKU of the image to be used for the VM"
-  type        = string
+variable "environment" {
+  description = "The environment for the VM (e.g., dev, test, prod)"
+  type = string
 }
 
 variable "admin_username" {
   description = "The admin username for the VM"
-  type        = string
+  type = string
 }
 
-variable "admin_password" {
-  description = "The admin password for the VM"
-  type        = string
+variable "ssh_public_key" {
+  description = "The public SSH key for authentication"
+  type = string
 }
 
-variable "size" {
-  description = "The size of the VM"
-  type        = string
+variable "image_publisher" {
+  description = "The publisher of the image to use"
+  type = string
 }
 
-variable "network_interface_ids" {
-  description = "The network interface IDs for the VM"
-  type        = list(string)
+variable "image_offer" {
+  description = "The offer of the image to use"
+  type = string
+}
+
+variable "image_sku" {
+  description = "The SKU of the image to use"
+  type = string
+}
+
+variable "os_disk_caching" {
+  description = "The caching for the OS disk (e.g., None, ReadOnly, ReadWrite)"
+  type = string
 }
