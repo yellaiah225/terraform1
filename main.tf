22c2741f253ebdb813ed92197fc6b9084fc2c4e7
yellaiah225 <137243250+yellaiah225@users.noreply.github.com>
2026-03-16 17:42:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -35,67 +35,7 @@ resource "azurerm_subnet" "my_subnet2" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 
-# Public IP
-resource "azurerm_public_ip" "vm_public_ip" {
-  name                = "${var.vm_name}-pip"
-  location            = data.azurerm_resource_group.existing_rg.location
-  resource_group_name = data.azurerm_resource_group.existing_rg.name
 
-  allocation_method = "Static"
-  sku               = "Standard"
-
-  tags = {
-    environment = var.environment
-  }
-}
-
-# Network Interface
-resource "azurerm_network_interface" "vm_nic" {
-  name                = "${var.vm_name}-nic"
-  location            = data.azurerm_resource_group.existing_rg.location
-  resource_group_name = data.azurerm_resource_group.existing_rg.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.my_subnet1.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.vm_public_ip.id
-  }
-
-  tags = {
-    environment = var.environment
-  }
-}
-
-
-resource "azurerm_linux_virtual_machine" "vm" {
-  name                = var.vm_name
-  resource_group_name = data.azurerm_resource_group.existing_rg.name
-  location            = data.azurerm_resource_group.existing_rg.location
-  size                = var.vm_size
-  admin_username      = var.admin_username
-  disable_password_authentication = true
-
-  network_interface_ids = [
-    azurerm_network_interface.vm_nic.id
-  ]
-
-  admin_ssh_key {
-    username   = var.admin_username
-    public_key = var.ssh_public_key
-  }
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
-    version   = "latest"
-  }
 
   tags = {
     environment = var.environment
