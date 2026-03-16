be0a8c89e4753d8ab8e7f3b2793a471ec74abb5a
yellaiah225 <137243250+yellaiah225@users.noreply.github.com>
2026-03-16 17:32:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -81,7 +81,10 @@ resource "azurerm_linux_virtual_machine" "vm" {
   network_interface_ids = [
     azurerm_network_interface.vm_nic.id
   ]
-
+   admin_ssh_key {
+    username   = var.admin_username
+    public_key = var.ssh_public_key
+  } 
 
 
   os_disk {
